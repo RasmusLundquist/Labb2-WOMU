03014c69eb329a97f2a8a1e0780f434e0bdaa568
RasmusLundquist <Rasmus.lundquist@hotmail.com>
2016-02-23 14:52:06 +0100

    Påbörjat WebApi

---
+++ Web-API/Models/Assignments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API.Models
+{
+    public class Assignments
+    {
+        public int UserID { get; set; }
+        public int TaskID { get; set; }
+    }
+}
---
+++ Web-API/Models/Tasks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API.Models
+{
+    public class Tasks
+    {
+        public int TaskID { get; set; }
+        public DateTime BeginDateTime { get; set; }
+        public DateTime DeadlineDateTime { get; set; }
+        public string Title { get; set; }
+        public string Requirements { get; set; }
+    }
+}
