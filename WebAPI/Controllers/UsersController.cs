b82350c33c551d3be0aaad04ac301707ef4e7cec
Thires Nilsson <s143979@student.hb.se>
2016-02-25 10:05:18 +0100

    hejsan

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Routing;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    /*
+    The WebApiConfig class may require additional changes to add a route for this controller. Merge these statements into the Register method of the WebApiConfig class as applicable. Note that OData URLs are case sensitive.
+
+    using System.Web.Http.OData.Builder;
+    using System.Web.Http.OData.Extensions;
+    using WebAPI.Models;
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Users>("Users");
+    builder.EntitySet<Tasks>("Tasks"); 
+    config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+    */
+    public class UsersController : ODataController
+    {
+        private DatabasEntities db = new DatabasEntities();
+
+        // GET: odata/Users
+        [EnableQuery]
+        public IQueryable<Users> GetUsers()
+        {
+            return db.Users;
+        }
+
+        // GET: odata/Users(5)
+        [EnableQuery]
+        public SingleResult<Users> GetUsers([FromODataUri] int key)
+        {
+            return SingleResult.Create(db.Users.Where(users => users.UserID == key));
+        }
+
+        // PUT: odata/Users(5)
+        public IHttpActionResult Put([FromODataUri] int key, Delta<Users> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Users users = db.Users.Find(key);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(users);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(users);
+        }
+
+        // POST: odata/Users
+        public IHttpActionResult Post(Users users)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Users.Add(users);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (UsersExists(users.UserID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Created(users);
+        }
+
+        // PATCH: odata/Users(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public IHttpActionResult Patch([FromODataUri] int key, Delta<Users> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Users users = db.Users.Find(key);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(users);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(users);
+        }
+
+        // DELETE: odata/Users(5)
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            Users users = db.Users.Find(key);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(users);
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // GET: odata/Users(5)/Tasks
+        [EnableQuery]
+        public IQueryable<Tasks> GetTasks([FromODataUri] int key)
+        {
+            return db.Users.Where(m => m.UserID == key).SelectMany(m => m.Tasks);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UsersExists(int key)
+        {
+            return db.Users.Count(e => e.UserID == key) > 0;
+        }
+    }
+}
