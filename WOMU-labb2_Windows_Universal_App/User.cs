f4b958d354d168beb9af2c6551f172b352d8667f
RasmusLundquist <Rasmus.lundquist@hotmail.com>
2016-02-25 13:57:41 +0100

    hej

---
+++ WOMU-labb2_Windows_Universal_App/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WOMU_labb2_Windows_Universal_App
+{
+    class User
+    {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
